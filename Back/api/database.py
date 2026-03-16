90a96ae306a51ec77905390e0cdbb66e877275c5
Wicmo <joel.edvardsson05@gmail.com>
2026-03-16 11:45:41 +0100

    supabase perp

--- Back/api/database.py
+++ Back/api/database.py
@@ -1,9 +1,7 @@
 import psycopg2
+import os
 
+def get_connection():
+    return psycopg2.connect(os.environ["DATABASE_URL"])
 
-conn = psycopg2.connect(
-    dbname="full_db",
-    user="postgres",
-    password="5DZ96PDs4U8aXs4eeTDGnVvQCwfzubJy2enxDhGw4dUHNv9wNMevUqEMQrXxxBnP",
-    host="localhost"
-)
+conn = get_connection()
